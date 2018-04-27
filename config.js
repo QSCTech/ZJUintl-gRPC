a286f68f8fc13b45fcd955ae0f3f3a0090c6e737
laphets <i@laphets.com>
2018-04-27 09:08:00 +0800

    update .gitignore

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    port: '0.0.0.0:50053',
-    test_user: {
-        username: '3170111705',
-        password: 'asdfghjkl'
-    }
-};
-
-module.exports = config;
--- test/test.js
+++
@@ -1,6 +0,0 @@
-new Promise((resolve, reject) => {
-    throw(233);
-    console.log(666);
-}).catch(err => {
-    console.log(err);
-})
