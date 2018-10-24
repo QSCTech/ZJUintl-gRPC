323d4b3ceeeb1648418f27c26b0c8f23771409ab
laphets <i@laphets.com>
2018-10-24 23:22:51 +0800

    Add whoami compose

--- client.js
+++ client.js
@@ -2,7 +2,7 @@
  * @Author: Laphets
  * @Date: 2018-04-22 00:42:03
  * @Last Modified by: Laphets
- * @Last Modified time: 2018-10-24 21:21:18
+ * @Last Modified time: 2018-10-24 23:18:00
  */
 
 const PROTO_PATH = __dirname + '/protos/zju_intl.proto';
@@ -11,29 +11,31 @@ const grpc = require('grpc');
 let protoDescriptor = grpc.load(PROTO_PATH);
 let zjuintl = protoDescriptor.zjuintl;
 const argv = require('minimist')(process.argv.slice(2));
-let client = new zjuintl.ZJUintl(`0.0.0.0:${argv['port'] || require('./config').port}`
+let client = new zjuintl.ZJUintl(`10.202.68.181:8890`
     , grpc.credentials.createInsecure());
 
 const user = require('./test_user').test_user;
 
 
 // Code for connect test
-// client.connect_test({name: 'lapehts'}, (err, response) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log(response);
-//     }
-// });
-
-//Code for get course(time table)
-client.getCourse({ username: user.username, password: user.password }, (err, response) => {
+client.connect_test({ name: 'lapehts' }, null, {
+    host: 'zjuintl.rpc.zjuqsc.com'
+},  (err, response) => {
     if (err) {
         console.log(err);
     } else {
         console.log(response);
     }
-})
+});
+
+//Code for get course(time table)
+// client.getCourse({ username: user.username, password: user.password }, (err, response) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(response);
+//     }
+// })
 
 // client.getBBGradeList({ username: user.username, password: user.password }, (err, response) => {
 //     if (err) {
