482e70dba9c74ff97c861e9f9ed20242096cd1c0
laphets <i@laphets.com>
2018-10-25 10:31:46 +0800

    Update network settings

--- client.js
+++ client.js
@@ -2,7 +2,7 @@
  * @Author: Laphets
  * @Date: 2018-04-22 00:42:03
  * @Last Modified by: Laphets
- * @Last Modified time: 2018-10-25 00:48:34
+ * @Last Modified time: 2018-10-25 07:55:54
  */
 
 const PROTO_PATH = __dirname + '/protos/zju_intl.proto';
@@ -17,17 +17,17 @@ let client = new qscrpc.ZJUintl(`10.202.68.181:8890`
 const user = require('./test_user').test_user;
 
 
-const meta = new grpc.Metadata()
-meta.add('Host', 'zjuintl.rpc.zjuqsc.com')
+// const meta = new grpc.Metadata()
+// meta.add('Host', 'zjuintl.rpc.zjuqsc.com')
 
 // Code for connect test
-client.connect_test({ name: 'lapehts' }, meta, (err, response) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(response);
-    }
-});
+// client.connect_test({ name: 'lapehts' }, (err, response) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(response);
+//     }
+// });
 
 //Code for get course(time table)
 // client.getCourse({ username: user.username, password: user.password }, (err, response) => {
@@ -38,13 +38,13 @@ client.connect_test({ name: 'lapehts' }, meta, (err, response) => {
 //     }
 // })
 
-// client.getBBGradeList({ username: user.username, password: user.password }, (err, response) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log(response);
-//     }
-// });
+client.getBBGradeList({ username: user.username, password: user.password }, (err, response) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(response);
+    }
+});
 
 // client.getBBCertainGrade({ username: user.username, password: user.password, courseid: user.courseid }, (err, response) => {
 //     if (err) {
